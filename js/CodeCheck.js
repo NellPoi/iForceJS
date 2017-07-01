2b7ca54ebfe3feb15df73bb596d87ca2d55ce8b7
DESKTOP-7I1N7G0\49017 <490177378@qq.com>
2017-07-01 13:49:18 +0800

    优化index文件在PHPstorm中换行格式，部署php开发环境

--- js/CodeCheck.js
+++ js/CodeCheck.js
@@ -4,9 +4,9 @@ var api_CodeCheck_CodeIinfo={
 var api_CodeCheck_Load=function(){
 	document.getElementById("Loading_DisplayRound").style.display="none";
 	/*#不支持版本向下兼容的代码
-	if(api_CheckCode_BasicStatus.Status=true){
-		var $toast_3 = $('<b class="white-text">api_CheckCode running</b>');
-		Materialize.toast($toast_3, 4000);
-		document.getElementById("ccid_BasicStatus").innerHTML="已被激活";
-	}*/
+	 if(api_CheckCode_BasicStatus.Status=true){
+	 var $toast_3 = $('<b class="white-text">api_CheckCode running</b>');
+	 Materialize.toast($toast_3, 4000);
+	 document.getElementById("ccid_BasicStatus").innerHTML="已被激活";
+	 }*/
 };
---
+++ public/helloworld.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: 49017
+ * Date: 2017/7/1
+ * Time: 13:37
+ */
+$var1="helloworld";
+echo($var1);
